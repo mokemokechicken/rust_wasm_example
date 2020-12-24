0fd38af6472a51f6f6989aa1919605d526e4b8c2
Ken Morishita <mokemokechicken@gmail.com>
2020-12-24 11:42:00 +0900

    example4b part2

--- example4b/src/lib.rs
+++ example4b/src/lib.rs
@@ -8,7 +8,7 @@ use web_sys::console::log_1;
 use core::cell::RefCell;
 use futures::future::join_all;
 use futures::prelude::*;
-use js_sys::Promise;
+use js_sys::{ArrayBuffer, Promise};
 use std;
 use std::rc::Rc;
 use wasm_bindgen_futures::{future_to_promise, JsFuture};
@@ -50,13 +50,13 @@ impl MyApp {
     // この関数自体を async にすると &mut selfの辺りが上手くいかない(確か)。
     // selfをClosureにcaptureさせずに 更新したい部分を Rc<RefCell> で capture させるのが良さそう。
     // この関数自体はPromiseをreturnさせる。
-    pub fn load_data(&mut self) -> Option<Promise> {
+    pub fn load_data(&mut self) -> Promise {
         let path_list = vec!["team1.json", "team2.json"];
         let mut future_list: Vec<_> = Vec::new();
         let ptr_for_update = self.teams.clone();
 
         for path in path_list {
-            let future = MyApp::json_request(path).and_then(|json| async move {
+            let future = MyApp::json_request(path, false).and_then(|json| async move {
                 let team: Team = json.into_serde().unwrap(); // team: Team と型を明示するのがポイント
                 Ok(team)
             });
@@ -68,12 +68,45 @@ impl MyApp {
                 teams.push(result_team.unwrap());
             }
             ptr_for_update.replace(Some(teams));
-            Ok(JsValue::from("99"))
+            Ok(JsValue::from("Success load_data()!"))
         });
-        Some(future_to_promise(future))
+        future_to_promise(future)
     }
 
-    fn json_request(path: &str) -> impl Future<Output = Result<JsValue, JsValue>> {
+    // ArrayBuffer の結合がしたかっただけ
+    pub fn load_data2(&mut self) -> Promise {
+        let path_list = vec!["team1.json", "team2.json"];
+        let mut future_list: Vec<_> = Vec::new();
+
+        for path in path_list {
+            let future = MyApp::json_request(path, true).and_then(|value| async move {
+                let ab: ArrayBuffer = value.dyn_into().unwrap();
+                Ok(ab)
+            });
+            future_list.push(future);
+        }
+        let future = join_all(future_list).map(move |vs: Vec<Result<ArrayBuffer, JsValue>>| {
+            let mut total_size: u32 = 0;
+            let mut array_buffers: Vec<ArrayBuffer> = Vec::new();
+            for result_ab in vs {
+                let ab: ArrayBuffer = result_ab.unwrap();
+                log(&format!("len={}", ab.byte_length()));
+                total_size += ab.byte_length();
+                array_buffers.push(ab);
+                // let type_buffer: js_sys::Uint8Array = js_sys::Uint8Array::new(&ab);
+            }
+            let buffer: js_sys::Uint8Array = js_sys::Uint8Array::new_with_length(total_size);
+            let mut pos: u32 = 0;
+            for ab in array_buffers {
+                buffer.set(&js_sys::Uint8Array::new(&ab), pos);
+                pos += ab.byte_length();
+            }
+            Ok(JsValue::from(buffer))
+        });
+        future_to_promise(future)
+    }
+
+    fn json_request(path: &str, as_array: bool) -> impl Future<Output = Result<JsValue, JsValue>> {
         let mut opts = RequestInit::new();
         opts.method("GET");
         opts.mode(RequestMode::Cors);
@@ -87,13 +120,19 @@ impl MyApp {
         let request_promise: Promise = window.fetch_with_request(&request);
         // ↑ ここまではお約束
 
+        let aa = as_array;
+
         // Promise -> Future を繰り返す感じになる。 web_sysの methodが Promiseを返すから?
         // Future は RustのNativeっぽい型、 JsFuture::from(promise) で Promise->Future に変換される
         let future = JsFuture::from(request_promise)
-            .and_then(|resp_value| async {
+            .and_then(move |resp_value| async move {
                 assert!(resp_value.is_instance_of::<Response>());
                 let resp: Response = resp_value.dyn_into().unwrap();
-                resp.json()
+                if aa {
+                    resp.array_buffer()
+                } else {
+                    resp.json()
+                }
             })
             .and_then(|value: Promise| JsFuture::from(value));
         future
